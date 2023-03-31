4c67680d43764081196796dfc410cc91205e46f2
polokir <lolak_228@ukr.net>
2023-03-31 11:34:38 +0300

    ready

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,40 +3,59 @@ import './App.css';
 import { CurrencyBox } from './CurrencyBox';
 
 const Header = () => {
-  const [fromCurrency,setFromCur] = useState('UAH')
-  const [toCurrency,setToCur] = useState('EUR')
-  const [fromValue,setFromVal] = useState(0);
-  const [toValue,setToVal] = useState(0);
+  const [fromCurrency, setFromCur] = useState('UAH');
+  const [toCurrency, setToCur] = useState('EUR');
+  const [fromValue, setFromVal] = useState(0);
+  const [toValue, setToVal] = useState(0);
   const rates = useRef({});
 
-  const onChangefromHandler = (value) => {
+  const onChangefromHandler = value => {
     const total = value / rates.current[fromCurrency];
     const result = total * rates.current[toCurrency];
     setFromVal(value);
     setToVal(result.toFixed(3));
-  }
+  };
 
-  const onChangetoHandler = (value) => {
-    const result = (rates.current[fromCurrency]/rates.current[toCurrency])*value
+  const onChangetoHandler = value => {
+    const result =
+      (rates.current[fromCurrency] / rates.current[toCurrency]) * value;
     setToVal(value);
     setFromVal(result.toFixed(3));
-  }
+  };
 
-  useEffect(()=>{
-    fetch('https://api.exchangerate.host/latest').then(res=>res.json())
-    .then(data => {
-      rates.current=data.rates;
-    })
-  },[])
+  useEffect(() => {
+    fetch('https://api.exchangerate.host/latest')
+      .then(res => res.json())
+      .then(data => {
+        rates.current = data.rates;
+      });
+  }, []);
 
-  useEffect(()=>{onChangefromHandler(fromValue)},[fromCurrency])
-  useEffect(()=>{onChangetoHandler(toValue)},[toCurrency])
+  useEffect(() => {
+    onChangefromHandler(fromValue);
+     // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [fromCurrency]);
+
+  useEffect(() => {
+    onChangetoHandler(toValue);
+     // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [toCurrency]);
   return (
     <div className="App">
-      <CurrencyBox value={fromValue} currency={fromCurrency} currencyHandler={setFromCur} valueHandler={onChangefromHandler}/>
-      <CurrencyBox value={toValue} currency={toCurrency} currencyHandler={setToCur} valueHandler={onChangetoHandler}/>
+      <CurrencyBox
+        value={fromValue}
+        currency={fromCurrency}
+        currencyHandler={setFromCur}
+        valueHandler={onChangefromHandler}
+      />
+      <CurrencyBox
+        value={toValue}
+        currency={toCurrency}
+        currencyHandler={setToCur}
+        valueHandler={onChangetoHandler}
+      />
     </div>
   );
-}
+};
 
 export default Header;
